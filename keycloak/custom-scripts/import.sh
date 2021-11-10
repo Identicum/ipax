4e1730952df7d5d742b678cb0cbcecd839b0637b
ggallard <ggallard@identicum.com>
2021-11-09 22:17:41 -0300

    variables

--- keycloak/custom-scripts/import.sh
+++ keycloak/custom-scripts/import.sh
@@ -2,16 +2,18 @@
 
 KCADM=$JBOSS_HOME/bin/kcadm.sh
 REALM_NAME=demorealm
+CLIENT_ID=ipax_client_id
+CLIENT_SECRET=ipax_client_secret
 USER_NAME=demo
 USER_PASS=demo
 
 for i in {1..10}; do
     $KCADM config credentials --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
-    custom_realm=$($KCADM get realms/demorealm)
+    custom_realm=$($KCADM get realms/$REALM_NAME)
     if [ -z "$custom_realm" ]; then
         $KCADM create realms -s realm="${REALM_NAME}" -s enabled=true -s registrationAllowed=true
 
-        $KCADM create clients -r $REALM_NAME -s clientId=ipax_client_id -s secret=ipax_client_secret -s 'redirectUris=["http://localhost/redirect_uri"]'
+        $KCADM create clients -r $REALM_NAME -s clientId=$CLIENT_ID -s secret=$CLIENT_SECRET -s 'redirectUris=["http://localhost/redirect_uri"]'
 
         $KCADM create users -r $REALM_NAME -s username=$USER_NAME -s enabled=true
         $KCADM set-password -r $REALM_NAME --username $USER_NAME --new-password $USER_PASS
