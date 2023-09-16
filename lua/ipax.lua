9889ddd6729120ef50c2ba4fd032b466b19f0ad8
ggallard <ggallard@identicum.com>
2023-09-16 20:36:09 -0300

    delete_account use case

--- lua/ipax.lua
+++ lua/ipax.lua
@@ -4,60 +4,55 @@
 
 -- get the Discovery metadata from the specified URL
 local function openidc_discover(url, ssl_verify, keepalive, timeout, exptime, proxy_opts, http_request_decorator)
-	log(DEBUG, "openidc_discover: URL is: " .. url)
-  
+	ngx.log(ngx.DEBUG, "openidc_discover: URL is: " .. url)
 	local json, err
 	local v = openidc_cache_get("discovery", url)
 	if not v then
-  
-	  log(DEBUG, "discovery data not in cache, making call to discovery endpoint")
-	  -- make the call to the discovery endpoint
-	  local httpc = http.new()
-	  openidc_configure_timeouts(httpc, timeout)
-	  openidc_configure_proxy(httpc, proxy_opts)
-	  local res, error = httpc:request_uri(url, decorate_request(http_request_decorator, {
-		ssl_verify = (ssl_verify ~= "no"),
-		keepalive = (keepalive ~= "no")
-	  }))
-	  if not res then
-		err = "accessing discovery url (" .. url .. ") failed: " .. error
-		log(ERROR, err)
-	  else
-		log(DEBUG, "response data: " .. res.body)
-		json, err = openidc_parse_json_response(res)
-		if json then
-		  openidc_cache_set("discovery", url, cjson.encode(json), exptime or 24 * 60 * 60)
+		ngx.log(ngx.DEBUG, "discovery data not in cache, making call to discovery endpoint")
+		-- make the call to the discovery endpoint
+		local httpc = http.new()
+		openidc_configure_timeouts(httpc, timeout)
+		openidc_configure_proxy(httpc, proxy_opts)
+		local res, error = httpc:request_uri(url, decorate_request(http_request_decorator, {
+			ssl_verify = (ssl_verify ~= "no"),
+			keepalive = (keepalive ~= "no")
+		}))
+		if not res then
+			err = "accessing discovery url (" .. url .. ") failed: " .. error
+			ngx.log(ngx.DEBUG, err)
 		else
-		  err = "could not decode JSON from Discovery data" .. (err and (": " .. err) or '')
-		  log(ERROR, err)
+			ngx.log(ngx.DEBUG, "response data: " .. res.body)
+			json, err = openidc_parse_json_response(res)
+			if json then
+				openidc_cache_set("discovery", url, cjson.encode(json), exptime or 24 * 60 * 60)
+			else
+				err = "could not decode JSON from Discovery data" .. (err and (": " .. err) or '')
+				ngx.log(ngx.DEBUG, err)
+			end
 		end
-	  end
-  
 	else
-	  json = cjson.decode(v)
+		json = cjson.decode(v)
 	end
-  
 	return json, err
-  end
-  
-  -- turn a discovery url set in the opts dictionary into the discovered information
-  local function openidc_ensure_discovered_data(opts)
+end
+
+-- turn a discovery url set in the opts dictionary into the discovered information
+local function openidc_ensure_discovered_data(opts)
 	local err
 	if type(opts.discovery) == "string" then
-	  local discovery
-	  discovery, err = openidc_discover(opts.discovery, opts.ssl_verify, opts.keepalive, opts.timeout, opts.discovery_expires_in, opts.proxy_opts,
-										opts.http_request_decorator)
-	  if not err then
-		opts.discovery = discovery
-	  end
+		local discovery
+		discovery, err = openidc_discover(opts.discovery, opts.ssl_verify, opts.keepalive, opts.timeout, opts.discovery_expires_in, opts.proxy_opts, opts.http_request_decorator)
+		if not err then
+			opts.discovery = discovery
+		end
 	end
 	return err
-  end
+end
 
-  local function get_first(table_or_string)
+local function get_first(table_or_string)
 	local res = table_or_string
 	if table_or_string and type(table_or_string) == 'table' then
-	  res = table_or_string[1]
+		res = table_or_string[1]
 	end
 	return res
 end
@@ -76,22 +71,22 @@ local function get_forwarded_parameter(headers, param_name)
 	local forwarded = get_first_header(headers, 'Forwarded')
 	local params = {}
 	if forwarded then
-	  local function parse_parameter(pv)
-		local name, value = pv:match("^%s*([^=]+)%s*=%s*(.-)%s*$")
-		if name and value then
-		  if value:sub(1, 1) == '"' then
-			value = value:sub(2, -2)
-		  end
-		  params[name:lower()] = value
+		local function parse_parameter(pv)
+			local name, value = pv:match("^%s*([^=]+)%s*=%s*(.-)%s*$")
+			if name and value then
+				if value:sub(1, 1) == '"' then
+					value = value:sub(2, -2)
+				end
+				params[name:lower()] = value
+			end
 		end
-	  end
-	  -- this assumes there is no quoted comma inside the header's value which should be fine as comma is not legal inside a node name, a URI scheme or a host name. The only thing that might bite us are extensions.
-	  local first_part = forwarded
-	  local first_comma = forwarded:find("%s*,%s*")
-	  if first_comma then
-		first_part = forwarded:sub(1, first_comma - 1)
-	  end
-	  first_part:gsub("[^;]+", parse_parameter)
+		-- this assumes there is no quoted comma inside the header's value which should be fine as comma is not legal inside a node name, a URI scheme or a host name. The only thing that might bite us are extensions.
+		local first_part = forwarded
+		local first_comma = forwarded:find("%s*,%s*")
+		if first_comma then
+			first_part = forwarded:sub(1, first_comma - 1)
+		end
+		first_part:gsub("[^;]+", parse_parameter)
 	end
 	return params[param_name:gsub("^%s*(.-)%s*$", "%1"):lower()]
 end
@@ -242,6 +237,7 @@ local function get_kc_user_action_url(kc_action)
 		redirect_uri = redirect_uri,
 		kc_action = kc_action
 	}
+	openidc_ensure_discovered_data(oidc_opts)
 	return oidc_opts.discovery.authorization_endpoint .. "?" .. ngx.encode_args(params)
 end
 
